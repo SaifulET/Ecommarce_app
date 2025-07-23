e90d7fd5cd4f330f617eed9ae694f0fa277bfb36
saiful <si912999@gmail.com>
2025-07-23 23:33:14 +0600

    test

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.css'
-import './assets/css/main.css'
-import './assets/css/animate.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import 'react-loading-skeleton/dist/skeleton.css'
-import App from './App.jsx'
-import axios from 'axios'
-axios.defaults.baseURL = 'http://localhost:5000';
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "./assets/css/main.css";
+import "./assets/css/animate.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "react-loading-skeleton/dist/skeleton.css";
+import App from "./App.jsx";
+import axios from "axios";
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
